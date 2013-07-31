129a0a1626eae6451bd3e0028ee35ae747e41e35
computaholic <computaholic@blackstorm.(none)>
2013-08-01 00:31:02 +0200

    ..

--- pivoice.pl
+++ pivoice.pl
@@ -2,3 +2,15 @@
 
 use warnings;
 use strict;
+use Config::IniFiles;
+
+print "Hello World\n";
+
+my %ini;
+tie %ini, 'Config::IniFiles', ( -file => "./default.config.ini" );
+
+my @secs=keys %{$ini{voice}};
+
+print "@secs\n";
+
+print Config::IniFiles::Sections;
