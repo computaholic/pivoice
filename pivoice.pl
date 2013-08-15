8ebee209a2d737535eddf100892b249b767f8f28
computaholic <2divine@gmx.net>
2013-08-15 23:55:29 +0200

    Basic functionality is given, but looping is missing. Would like to use array as system() input, need an intelligent splitting algo

--- pivoice.pl
+++ pivoice.pl
@@ -16,16 +16,21 @@ my %scn_all;				# all available scenarios
 my $scn_default="default";# default scenario which provides parameter 
 							# values, if not present in other scenario
 
+my $command_default="default"; 	# default command in dictionary
+
 my $scn_start;				# starting scenario, [*scenario] 
 my $scn_current;			# current scenario
 
-my %dict_all;				# all dictionaries
+my %dict_all;				# hash of all dicts, sorted by scenario
 
-my $dict_current; 		# current dictionary;
-my $dict_default="default"; # default command in dictionary
+#my $command_current; 				# current command of current dictionary;
+									# current dict is a hash:
+									# $dict_all{$scn_current}
 
-my $debug=1;				# debuglevel
-
+my $debug		= 1;				# debuglevel
+my $prog_name 	= "pivoice.pl";
+my $func_name 	= "main";
+my $deb_th 	= 1;
 # ----------------------------------------------------------------------
 # Prototypes
 # ----------------------------------------------------------------------
@@ -33,6 +38,9 @@ sub expand_special_expression($$$\%\%);
 sub scenario_get_start(\%);
 sub dict_get_all(\%);
 sub gen_regex_from_simple($$);
+sub get_voice();
+sub split_action($);
+sub print_debug($$$$$);
 
 ########################################################################
 ########################################################################
@@ -42,52 +50,62 @@ sub gen_regex_from_simple($$);
 # main program
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
+print_debug("ENTERING $func_name", $prog_name, $func_name, 1, 1);
 
+# ----------------------------------------------------------------------
+# Loading config and dictionaries
+# ----------------------------------------------------------------------
 
 # loading config into tied hash scn_all
 tie %scn_all, 'Config::IniFiles', ( -file => "./pivoice.conf", -default => $scn_default );
+print_debug("Loading config file successfull", $prog_name, $func_name, 1, 0);
+
+# %dict_all is a hash of hashes (one for each scenario)
+# these are also hashes of hashes (one for each command)
+%dict_all = dict_get_all(%scn_all);
+print_debug("Loading dictionaries successfull", $prog_name, $func_name, 1, 0);
+
+
+# ----------------------------------------------------------------------
+# Setting up starting conditions
+# ----------------------------------------------------------------------
 
 # find the starting scenario that starts with *
 $scn_start = scenario_get_start(%scn_all);
 
-# loading all dictionaries into one hash
-%dict_all = dict_get_all(%scn_all);
-
-
-
-# For now we will fake an input string
-#my $INPUT="would you play wow from bANg and so on\n";
-print "Enter INPUT: ";
-my $INPUT= <>;
-
-# now removing blanks at the beginning and the end
-chomp ($INPUT);
-$INPUT =~ s/^(( )*)?//;
-$INPUT =~ s/( )*?$//;
-
-
-my $test = expand_special_expression("!say", $scn_start, "playsong", %scn_all, %dict_all);
-
 # set current scenario to start scenario
 $scn_current=$scn_start;
+
+#~~~~ test ~~~~~# 
+#my $test = expand_special_expression("!say", $scn_start, "playsong", %scn_all, %dict_all);
+#~~~~ test ~~~~~#
+print keys $dict_all{$scn_current};
+my $INPUT = get_voice();
+
+# ----------------------------------------------------------------------
+# Main Loop
+# ----------------------------------------------------------------------
 
 # going over all commands that are in the dict of current scenario
 for ( keys $dict_all{$scn_current} )
 {
+	print_debug( "Current scenario is $scn_current", $prog_name, $func_name, $deb_th, 0);
+	# now $_ holds the current command, but	
 	# we do not care for the default command
-	if ( $_ ne $dict_default )
+	if ( $_ ne $command_default )
 	{
 		my $command 		= $_;												# current command
 		my $listen			= $dict_all{$scn_current}{$_}{"ListenFor"};		# matching string, what we listen for
 		my $matchstyle 	= $dict_all{$scn_current}{$_}{"MatchStyle"}; 	# matching style (simple|regex)
 		my $action 		= $dict_all{$scn_current}{$_}{"Action"};			# action to do
+		my $scn_next;															# Next Scenario
 		
-		print "pivoice.pl:\tChecking command <$command> with <$listen>\n" if ($debug);
+		print_debug( "Checking command <$command> with <$listen>", $prog_name, $func_name, $deb_th, 0);
 		
 		# lets check the MatchStyle (this is an ifthenelse-war, but perl
 		# does not consistently support switch statements)
 		
-		print "pivoice.pl:\tMatch Type for command $command is $matchstyle\n" if ($debug);
+		print_debug( "Match Type for command $command is $matchstyle", $prog_name, $func_name, $deb_th, 0);
 		
 		if ( $matchstyle eq "simple" )
 		{
@@ -96,44 +114,87 @@ for ( keys $dict_all{$scn_current} )
 			
 			# now put that regex back in and change MatchType, so we 
 			# dont have to do that again
-			$dict_all{$scn_current}{$command}{"ListenFor"} = $listen;
-			$dict_all{$scn_current}{$command}{"Action"} = $action;
-			$dict_all{$scn_current}{$command}{"MatchStyle"} = "regex";
-			
-			# updating help variables (not all needed, but lets keep that for now)
-			$listen 		= $dict_all{$scn_current}{$command}{"ListenFor"};
-			$matchstyle 	= $dict_all{$scn_current}{$command}{"MatchStyle"};
-			$action 		= $dict_all{$scn_current}{$command}{"Action"};
+			$dict_all{$scn_current}{$command}{"ListenFor"} 	= $listen;
+			$dict_all{$scn_current}{$command}{"Action"} 	= $action;
+			$dict_all{$scn_current}{$command}{"MatchStyle"}	= "regex";
 		}
 		
 		# matching regex against $INPUT
 		if ( $INPUT =~ /$listen/ )
 		{
+			# ================================ #
+			# This gets executed if we have a  #
+			# match, just if you didnt notice  #
+			# ================================ #
+			
+			# get a list of variables in $INPUT
 			my  @match_list = ( $INPUT =~ /$listen/ );
-			print "pivoice.pl:\tVoice command |$INPUT| matches $listen\n" if ($debug);
-			print "pivoice.pl:\tCommand has ", scalar @match_list, " parameters: " if ($debug);
-			print "$_ " for @match_list;
-			print "\n" if ($debug);
 			
-			# replacing $1, $2... in $action with actual values
-			my $varcount=1;
-			for ( @match_list)
+			# DEBUG
+			print_debug( "Voice command |$INPUT| matches $listen", $prog_name, $func_name, $deb_th, 0);
+			my $count = scalar @match_list;
+			print_debug( "Command has $count parameters: ", $prog_name, $func_name, $deb_th, 0);
+			for ( @match_list )
 			{
-				$action =~ s/(\$$varcount)/$match_list[($varcount-1)]/;
-				$varcount++;
+				print_debug( "$_ ", $prog_name, $func_name, $deb_th, 0);
 			}
+			# DEBUG END
 			
-			print "pivoice.pl:\tAction>>>>>>\n\n" if ($debug);
-			system($action);
-			print "\npivoice.pl:\tAction<<<<<<\n\n\n" if ($debug);
+			# the $action string will be split up, then every part will 
+			# be expanded, then the variables will be replaced.
+			
+			print_debug( "Splitting, expansion and substitution in \$action: |$action|", $prog_name, $func_name, $deb_th, 0);
+			my @action_array = split(' ', $action); #need better split command
+			#my @action_array = split_action($action);
+			for ( @action_array )
+			{ 
+				# expand special expressions
+				$_ = expand_special_expression($_, $scn_current, $command, %scn_all, %dict_all);
+
+				# replace found variables
+				$_ =~ s/\$(\d+)/$match_list[($1-1)]/g;
+			}
+			print_debug( "\$action is now ", $prog_name, $func_name, $deb_th, 0);
+			print_debug( join(' ', @action_array), $prog_name, $func_name, $deb_th, 0);
+			
+			print_debug( "Action>>>>>>\n", $prog_name, $func_name, $deb_th, 0);
+			
+			# performing action
+			system(join(' ', @action_array));
+			
+			print_debug( " ", $prog_name, $func_name, $deb_th, 0);
+			print_debug( "Action<<<<<<", $prog_name, $func_name, $deb_th, 0);
+			
+			# Next Scenario can be set per command or per scenario
+			if (defined $dict_all{$scn_current}{$_}{"NextScenario"} )
+			{
+				$scn_next = $dict_all{$scn_current}{$_}{"NextScenario"};
+			}
+			elsif ( defined $scn_all{$scn_current}{"NextScenario"} )
+			{
+				$scn_next = $scn_all{$scn_current}{"NextScenario"};
+			}
+			else
+			{
+				print "Next scenario definition is missing\n";
+			}
+			print_debug( "Next scenario is $scn_next", $prog_name, $func_name, $deb_th, 0);
+			$scn_current = $scn_next;
+			last;
 		} 
 		else
 		{
-			print "pivoice.pl:\tVoice command |$INPUT| does not match |$listen|\n\n" if ($debug);
+			print_debug( "Voice command |$INPUT| does not match |$listen|\n", $prog_name, $func_name, $deb_th, 0);
 		}
 	}
+	else
+	{
+		print_debug( "Skipping command |$_|", $prog_name, $func_name, $deb_th, 0);
+	}
+	
 }
 
+print_debug( "LEAVING $func_name", $prog_name, $func_name, $deb_th, 0);
 ########################################################################
 ########################################################################
 
@@ -148,6 +209,12 @@ for ( keys $dict_all{$scn_current} )
 # ----------------------------------------------------------------------
 sub expand_special_expression($$$\%\%)
 {
+	# ~
+	my $func_name = "expand_special_expression";
+	my $deb_th 	= 1;
+	print_debug( "ENTERING $func_name", $prog_name, $func_name, $deb_th, 1);
+	# ~
+	
 	my $exp				= shift;	# !expression
 	my $current_scenario	= shift;	# current scenario
 	my $current_command	= shift;
@@ -166,28 +233,37 @@ sub expand_special_expression($$$\%\%)
 	# will automatically check for the default value if not given in 
 	# current (see tie %.. command)
 	
-	print "pivoice.pl:\tExpanding expression $exp\n" if ($debug);
+	print_debug( "Expanding expression |$exp|", $prog_name, $func_name, $deb_th, 0);
+	print_debug( "Current scenario is |$current_scenario|", $prog_name, $func_name, $deb_th, 0);
+	print_debug( "Current command is |$current_command|", $prog_name, $func_name, $deb_th, 0);
+	
+	
 	while ( $exp =~ /\!(\w+)/ )
 	{
-	for ( $exp =~ /\!(\w+)/ )
-	{
-		print "pivoice.pl:\t.. $_ ..\n" if ($debug);
-		# $_ holds current !exp name without '!'
-		if ( defined $$dicts{$current_scenario}{$current_command}{$_} )
+		print_debug( "Expression |$exp| is a special command", $prog_name, $func_name, $deb_th, 0);
+	
+		for ( $exp =~ /\!(\w+)/ )
 		{
-			# replace found !.. by dict
-			$exp =~ s/\!(\w+)/$$dicts{$current_scenario}{$current_command}{$1}/;
-		}
-		elsif ( defined $$scenarios{$current_scenario}{$_} )
-		{
-			# replace found !.. by scenario
-			$exp =~ s/\!(\w+)/$$scenarios{$current_scenario}{$1}/;
-		}
-	} 
+			# $_ holds current !exp name without '!'
+			if ( defined $$dicts{$current_scenario}{$current_command}{$_} )
+			{
+				# replace found !.. by dict
+				$exp =~ s/\!(\w+)/$$dicts{$current_scenario}{$current_command}{$1}/;
+				print_debug( "Rule by command: |$exp|", $prog_name, $func_name, $deb_th, 0);
+			}
+			elsif ( defined $$scenarios{$current_scenario}{$_} )
+			{
+				# replace found !.. by scenario
+				$exp =~ s/\!(\w+)/$$scenarios{$current_scenario}{$1}/;
+				print_debug( "Rule by scenario: |$exp|", $prog_name, $func_name, $deb_th, 0);
+			}
+		} 
+		
 	}
 
 	# return expanded command
-	print "pivoice.pl:\tExpanded to: $exp\n" if ($debug);
+	print_debug( "Expanded to: |$exp|", $prog_name, $func_name, $deb_th, 0);
+	print_debug( "LEAVING $func_name", $prog_name, $func_name, $deb_th, 0);
 	return $exp;
 }
 
@@ -209,12 +285,17 @@ sub gen_regex_from_simple($$)
 	# ListenFor = google @all
 	# Action    = script_to_google "@all"
 	
+	# ~
+	my $func_name = "gen_regex_from_simple";
+	my $deb_th 	= 1;
+	print_debug( "ENTERING $func_name", $prog_name, $func_name, $deb_th, 1);
+	# ~
+	
 	my $listen = shift;
 	my $action = shift;
 	
-	print "pivoice.pl:\t\$listen: $listen" if ($debug);
-	print "pivoice.pl:\t\$action: $action" if ($debug);
-	
+	print_debug( "\$listen: $listen", $prog_name, $func_name, $deb_th, 0);
+	print_debug( "\$action: $action", $prog_name, $func_name, $deb_th, 0);
 	
 	# cleaning up the listen words
 	chomp($listen);
@@ -226,14 +307,16 @@ sub gen_regex_from_simple($$)
 	my $mode="unset";
 	
 	# first replace all @ by $_, this helps me to sort things easier
-	print "pivoice.pl:\tReplacing @ by \$_\n" if ($debug);
+	print_debug( "Replacing @ by \$_", $prog_name, $func_name, $deb_th, 0);
+	
 	$listen =~ s/@(\w+?)/\$_$1/;
 	$action =~ s/@(\w+?)/\$_$1/;
-	print "pivoice.pl:\t\$listen: $listen" if ($debug);
-	print "pivoice.pl:\t\$action: $action" if ($debug);
 	
-	#print "pivoice.pl:\tAll vars found: ", ( $listen =~ /(\$\w*)?|(\@\w*)?/g ), "\n" if ($debug);
-	for ( $listen =~ /\$(\w*)?/g )
+	print_debug( "\$listen: $listen", $prog_name, $func_name, $deb_th, 0);
+	print_debug( "\$action: $action", $prog_name, $func_name, $deb_th, 0);
+	
+	#print "All vars found: ", ( $listen =~ /(\$\w*)?|(\@\w*)?/g ), "\n" if ($debug);
+	for ( $listen =~ /\$(\w+)?/g )
 	{
 		# thats the first match in $listen
 		$varname = $_;
@@ -241,16 +324,15 @@ sub gen_regex_from_simple($$)
 		# checking if scalar or array variable
 		$mode = "scalar" if ( $varname =~ /^[^\_]/ );
 		$mode = "array" if ( $varname =~ /^\_/ );
-		print "pivoice.pl:\tDetected mode is $mode\n" if ($debug);
-		
-		print "pivoice.pl:\tReplacing \$ and \$_ vars\n" if ($debug);
-		print "pivoice.pl:\tFound varname \$$_\n" if ($debug);
-		print "pivoice.pl:\tCurrent action is $action\n" if ($debug);
+		print_debug( "Detected mode is $mode", $prog_name, $func_name, $deb_th, 0);
+		print_debug( "Replacing \$ and \$_ vars", $prog_name, $func_name, $deb_th, 0);
+		print_debug( "Found varname \$$_", $prog_name, $func_name, $deb_th, 0);
+		print_debug( "Current action is $action", $prog_name, $func_name, $deb_th, 0);
 		
 		# replace varnames in $action to $1, $2... 
 		$action =~ s/(\$$varname)/\$$counter/g;
 		
-		print "pivoice.pl:\tNew action is $action\n" if ($debug);
+		print_debug( "New action is $action", $prog_name, $func_name, $deb_th, 0);
 		
 		# counting one up, so we know what ${num} ($1, $2,...} is
 		$counter++;
@@ -258,26 +340,28 @@ sub gen_regex_from_simple($$)
 	
 	
 	# after replacing varnames in $action we gen. regex in $listen
-	$listen =~ s/\$[^_](\w*)?/\(\\w\*\)\?/g;
-	print "pivoice.pl:\tregex 1st run: $listen\n" if ($debug);
+	$listen =~ s/\$[^_](\w+)?/\(\\w\+\)\?/g;
+	print_debug( "regex 1st run: $listen", $prog_name, $func_name, $deb_th, 0);
 	
-	$listen =~ s/\$[\_](\w*)?/\(\.\*\)\?/g;
-	print "pivoice.pl:\tregex 2nd run: $listen\n" if ($debug);
+	$listen =~ s/\$[\_](\w+)?/\(\.\+\)\?/g;
+	print_debug( "regex 2nd run: $listen", $prog_name, $func_name, $deb_th, 0);
 	
 	# checking for absolute or non absulte match
 	if ( ($listen =~ /^~/) )
 	{
-		print "pivoice.pl:\tUsing NON absulute match\n" if ($debug);
+		print_debug( "Using NON absulute match", $prog_name, $func_name, $deb_th, 0);
 		$listen =~ s/(^~( *))//;
 	}
 	else
 	{
-		print "pivoice.pl:\tUsing absulute match\n" if ($debug);
+		print_debug( "Using absulute match", $prog_name, $func_name, $deb_th, 0);
 		$listen = "^$listen\$";
 	}
 	
-	print "pivoice.pl:\tGenerated regex $listen\n" if ($debug);
+	print_debug( "Generated regex $listen", $prog_name, $func_name, $deb_th, 0);
 	
+	
+	print_debug( "LEAVING $func_name", $prog_name, $func_name, $deb_th, 0);
 	return ($listen, $action);
 }
 
@@ -286,6 +370,12 @@ sub gen_regex_from_simple($$)
 # ----------------------------------------------------------------------
 sub dict_get_all(\%)
 {
+	# ~
+	my $func_name = "dict_get_all";
+	my $deb_th = 1;
+	# ~
+	print_debug( "ENTERING $func_name", $prog_name, $func_name, $deb_th, 1);
+	
 	my $scenarios = shift;
 	
 	my %dict_files;		# hash of all detected files
@@ -300,35 +390,134 @@ sub dict_get_all(\%)
 			
 			# tie each file to a temporary hash and save the reference
 			# to that hash in a different hash
-			tie my %tmp_dict, 'Config::IniFiles', ( -file => "$dict_files{$_}", -default => $dict_default );
+			tie my %tmp_dict, 'Config::IniFiles', ( -file => "$dict_files{$_}", -default => $command_default );
 			$dict_all{$_} = \%tmp_dict;
 			
 			# debug information
-			print "pivoice.pl:\tTied ", \%tmp_dict, " to file $dict_files{$_}\n\n" if ($debug);
+			print_debug( "Tied $dict_all{$_} to file $dict_files{$_}", $prog_name, $func_name, $deb_th, 0);
+			
 		}
 	}
 	
+	#print keys $dict_all{"*keyword"};
 	# return hash with all dictionaries
+	print_debug( "LEAVING $func_name", $prog_name, $func_name, $deb_th, 0);
 	return %dict_all;
 }
 
+# ----------------------------------------------------------------------
 # this will return the starting scenario
+# ----------------------------------------------------------------------
 sub scenario_get_start(\%)
 {
+	# ~
+	my $func_name = "scenario_get_start";
+	my $deb_th = 1;
+	# ~
+	
+	print_debug( "ENTERING $func_name", $prog_name, $func_name, $deb_th, 1);
+	
 	my $scenarios = shift;
 	for (keys %$scenarios)
 	{
 		if (/^\*/) # probing if first character is '*'
 		{
-			print "pivoice.pl:\tStarting scenario is $_\n\n" if ($debug);
+			print_debug( "Starting scenario is $_", $prog_name, $func_name, $deb_th, 0);
 			return $_;
 		}
 	}
 	
 	# if this gets executed, then no starting scenario was found
-	print "pivoice.pl:\tNo Starting scenario found\n\n" if ($debug);
+	print_debug( "No Starting scenario found", $prog_name, $func_name, $deb_th, 0);
 	
+	print_debug( "LEAVING $func_name", $prog_name, $func_name, $deb_th, 0);
 	return 1; 
+}
+
+# ----------------------------------------------------------------------
+# turn the mic on and get started:)
+# ----------------------------------------------------------------------
+sub get_voice()
+{
+	my $voice_string;
+	
+	# ~
+	my $func_name = "scenario_get_start";
+	my $deb_th = 1;
+	# ~
+	
+	# For now we will fake an input string
+	#my $INPUT="would you play wow from bANg and so on\n";
+	print "Enter INPUT: ";
+	$voice_string= <>;
+
+	# now removing blanks at the beginning and the end
+	chomp ($voice_string);
+	$voice_string =~ s/^(( )*)?//;
+	$voice_string =~ s/( )*?$//;
+	
+	return $voice_string;
+}
+
+# ----------------------------------------------------------------------
+# splitting action into right arrays
+# ----------------------------------------------------------------------
+sub split_action($)
+{
+	#~ # Basically, we need to seperate !subs and ".." and '..' strings
+	#~ my $tmp_action = shift;
+	#~ my @tmp_array;
+	#~ my @action_array;
+	#~ 
+	#~ # seperate by !subs
+	#~ while ( $tmp_action =~ /\!(\w+)/ )
+	#~ {
+		#~ @tmp_array 		= ( $tmp_action =~ /^(.*)?(\!\w+)(.*)$/ );
+		#~ push(@action_array, $tmp_array[1]) if ( $tmp_array(1) ne  "" );
+		#~ push(@action_array, $tmp_array[2]) if ( $tmp_array(3) ne  "" );
+		#~ push(@action_array, $tmp_array[3]) if ( $tmp_array(3) ne  "" );
+		#~ 
+		#~ $tmp_action 	= $tmp_array[3];
+	#~ }
+	#~ 
+	#~ # seperate by " "
+	#~ while ( $tmp_action =~ /\"(\w+)\"/ )
+	#~ {
+		#~ @tmp_array 		= ( $tmp_action =~ /^(.*)?(\"\w+\")(.*)$/ );
+		#~ push(@action_array, $tmp_array[1]) if ( $tmp_array(1) ne  "" );
+		#~ push(@action_array, $tmp_array[2]) if ( $tmp_array(3) ne  "" );
+		#~ push(@action_array, $tmp_array[3]) if ( $tmp_array(3) ne  "" );
+		#~ 
+		#~ $tmp_action 	= $tmp_array[3];
+	#~ }
+	#~ 
+	#~ return @tmp_array;
+}
+
+# ----------------------------------------------------------------------
+# printing debug messages
+# ----------------------------------------------------------------------
+sub print_debug($$$$$)
+{
+	exit 0 unless ( $debug );
+	
+	my $msg				= shift;
+	my $prog_name			= shift;
+	my $func_name			= shift;
+	my $debug_threshold 	= shift;
+	my $print_names 		= shift;
+	
+	if ($debug >= $debug_threshold )
+	{
+			if ( $print_names )
+		{
+			print "\n$prog_name", "::", "$func_name", "\n\t\t", "$msg\n";
+		}
+		else
+		{
+			print "\t\t", "$msg\n";
+		}
+	}
 }
 
 
